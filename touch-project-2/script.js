3a13b31f1de330b0046071012173832959b2d2c7
Zbigniew Nędzi <znedzi@wp.pl>
2024-12-30 23:15:04 +0100

    Projekt 2-do poprawek

---
+++ touch-project-2/script.js
@@ -0,0 +1,54 @@
+'use strict'
+
+function showResult(result){
+    alert('Result is: ' + result)
+}
+function getNumberFormUser(){
+    var input = prompt('Type number!')
+    return Number(input)
+    
+}
+function getOperatorFromUser(){
+    return prompt('Type operator (+, -, *, /)' )
+}
+function getConfirmationFromUser(){
+    return confirm('Do you wat to proceed?')
+}
+function getConfirmationFromUserAndProced(){
+    var confirmation = getConfirmationFromUser()
+
+//Jeśli użytkownik wykonuje kolejne ioperacje to wchodzimy w mainLoop, jeśli tylko jedną to wyświetlamy showResult
+
+    if(confirmation){
+        mainLoop(result)
+    } else {
+        showResult(result)
+    }
+}
+function calculate(operator, a, b){
+    var calculation = 0
+
+    switch(operator){
+        case '+':
+            return a + b
+        case '-':
+            return a - b
+        case '*':
+            return a * b
+        case '/':
+            return a / b
+    }
+}
+
+function mainLoop(result){
+    var operator = getOperatorFromUser()
+    var number = getNumberFormUser()
+
+    var newResult = calculate(operator, result, number)
+
+    getConfirmationFromUserAndProced(newResult)
+}
+
+var result = getNumberFormUser()
+
+getConfirmationFromUserAndProced(result)
