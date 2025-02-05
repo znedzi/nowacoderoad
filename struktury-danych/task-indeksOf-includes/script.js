e6a2f34ec28f1b9e04f7fe013a5d54769bafa1d7
Zbigniew Nędzi <znedzi@wp.pl>
2025-02-05 22:21:49 +0100

    task-function indeksOf and includes.

--- struktury-danych/task-indeksOf-includes/script.js
+++ struktury-danych/task-indeksOf-includes/script.js
@@ -17,3 +17,18 @@ const checkIfArrayIncludes = function(array, needle) {
 
 console.log(checkIfArrayIncludes(names, 'Ala')) //true
 console.log(checkIfArrayIncludes(names, 'Iza')) //false
+
+// funkcja indexOf() wyszukaj index jest wbudowana w tablicę i w przypadku trafienia
+// zwraca index liczbowy tablicy, a w przypadku nietrafienia wartość -1
+console.log(names.indexOf('Ala')) //0
+console.log(names.indexOf('Ola')) //1
+console.log(names.indexOf('Ela')) //2
+console.log(names.indexOf('Iza')) // -1 (nie odnaleziono)
+console.log(names.indexOf('Ala', 2)) //szukaj od index-u 2, -1(nie odnaleziono)
+
+console.log(names.indexOf('Ala') !== -1) //true
+console.log(names.indexOf('Iza') !== -1) //false
+
+console.log('includes: ' + names.includes('Ala')) //true
+console.log('includes: ' + names.includes('Iza')) //false
+console.log('includes: ' + names.includes('Ala', 2)) //false (szukaj od indeksu 2)
