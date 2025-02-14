f4b685eaea4ef7a9712e6ccf4961390a5c1f6127
Zbigniew Nędzi <znedzi@wp.pl>
2025-02-15 00:19:33 +0100

    Task higher order functions

---
+++ struktury-danych/task-higher-order-functions/script.js
@@ -0,0 +1,45 @@
+//  Przykład nr 1
+
+// const hello = function (name){
+//     console.log('Hello ' + name)
+// }
+
+// const makeHello = function(){
+//     return hello
+// }
+
+// makeHello()('Janek')
+// makeHello()('Maciek')
+// makeHello()('Zbyszek')
+
+// // Przykład nr 2
+
+// const makeHello2 = function() {
+//     return function(){
+//         console.log('Hello world!')
+//     }
+// }
+
+// const hello2 = makeHello2()
+
+// hello2()
+
+const callWithNumber2 = function(fn){
+    return fn(2)
+}
+
+const add2 = function(x){
+    return x + 8
+}
+
+// callWithNumber2 z parametrem add2 zwróci 10
+// gdyż jako parametr jest wstawiana funkcja 
+console.log(
+    callWithNumber2(add2)
+)
+
+
+// callWithNumber2 po przekazaniu funkcji consol log 
+// podstawi jako parametr 2, która zostanie wyświetlona
+// gdyz jako parametr jest wstawiana funkcja console.log
+callWithNumber2(console.log) 
