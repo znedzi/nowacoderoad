e93759869dfcc7794b69f8deec7302560838522f
Zbigniew Nędzi <znedzi@wp.pl>
2025-02-18 21:32:40 +0100

    Task-15. Filter complete

---
+++ struktury-danych/task-15-filter/script.js
@@ -0,0 +1,47 @@
+const numbers = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+
+const positiveNumbers = numbers.filter(function(number, index, array){
+    return number > 0
+})
+
+
+
+
+const negativeNumbers = numbers.filter(function(number, index, array){
+    return number < 0
+})
+
+console.log(positiveNumbers)
+console.log(negativeNumbers)
+
+
+const  myOwnFilter = function (array, callback){
+
+    let filteredArray = []
+
+    for(let i = 0; i < array.length; i++) {
+
+        const element = array[i]
+        const index = i
+
+        const result = callback(element, index, array)
+
+        if(result){
+            // poszczególne wyniki dodajemy do tablicy
+            filteredArray = filteredArray.concat(element) 
+        }
+    }
+    // zwracamy przefiltrowaną tablicę
+    return filteredArray
+}
+
+const positiveNumbersByMyOwnFilter = myOwnFilter(numbers,  function(number, index, array){
+    return number > 0
+})
+
+const negativeNumbersByMyOwnFilter = myOwnFilter(numbers,  function(number, index, array){
+    return number < 0
+})
+
+console.log(positiveNumbersByMyOwnFilter)
+console.log(negativeNumbersByMyOwnFilter)
