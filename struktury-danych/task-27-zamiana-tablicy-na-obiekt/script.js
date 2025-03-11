de3c1876c9f2afff68ee545499031fedf0321120
Zbigniew Nędzi <znedzi@wp.pl>
2025-03-11 23:39:42 +0100

    Task-27.Funkcja dziłająca z kluczem o dowolnej nazwie. Zamiana tablicy na obiekt.

--- struktury-danych/task-27-zamiana-tablicy-na-obiekt/script.js
+++ struktury-danych/task-27-zamiana-tablicy-na-obiekt/script.js
@@ -5,19 +5,25 @@ const people = [
     {id: '00004', name: 'Iza', age: 24 },
 ]
 
-const peopleObject = people.reduce(
-    
-    function(reduced, person, index,array){
-        const key = person.id
-        const value = person
 
-        // delete służy do usuwania wartości z obiektu
-        delete value.id
-        
-        reduced[key] = value
-        
-        return reduced
-    },{}
-)
+
+const arrayToObject = function(array, keyPropertyName = 'id') {
+    return array.reduce(
+    
+        function(reduced, person, index,array){
+            const key = person[keyPropertyName]
+            const value = person
+    
+            // delete służy do usuwania wartości z obiektu
+            delete value[keyPropertyName]
+            
+            reduced[key] = value
+            
+            return reduced
+        },{}
+    )
+}
+
+const peopleObject = arrayToObject(people, 'id')
 
 console.log(peopleObject)
